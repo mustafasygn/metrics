115ef3916e2b66c9cbd7f43f30d06f5a4d6669fc
mustafa saygin <mustafasygn@gmail.com>
2017-07-12 02:56:37 +0300

    removing old methods

--- src/main/java/com/n26/metrics/data/MetricRepository.java
+++ src/main/java/com/n26/metrics/data/MetricRepository.java
@@ -46,44 +46,8 @@ public class MetricRepository {
 		Long transactionSecond = TimeUnit.MILLISECONDS.toSeconds(transaction.getTimestamp());
 		Long second = (System.currentTimeMillis() - transaction.getTimestamp()) / MILLIS_TO_SECOND;
 		validateBySecond(second, secondLimit);
-		//		updateStatisticsByNewAmount(amount);
 		ArrayList<Double> amountList = ALL_TRANSACTIONS.getOrDefault(second, new ArrayList<>());
 		amountList.add(transaction.getAmount());
 		ALL_TRANSACTIONS.put(transactionSecond, amountList);
 	}
-
-	//	public synchronized static void updateStatisticsByNewAmount(Double amount) {
-	//		statistic.setCount(statistic.getCount() + 1L);
-	//		statistic.setSum(statistic.getSum() + amount);
-	//		statistic.setAvg(statistic.getSum() / statistic.getCount());
-	//		if (statistic.getMin() > amount) {
-	//			statistic.setMin(amount);
-	//		}
-	//		if (statistic.getMax() < amount) {
-	//			statistic.setMax(amount);
-	//		}
-	//	}
-
-	//	public synchronized static void updateStatisticsAfterOneSecond() {
-	//		ArrayList<Double> lastSecondAmounts = ALL_TRANSACTIONS.get(SECOND_LIMIT - 1);
-	//		statistic.setCount(statistic.getCount() - lastSecondAmounts.size());
-	//		Double sumLastSecondAmounts = lastSecondAmounts.stream().mapToDouble(Double::doubleValue).sum();
-	//		statistic.setSum(statistic.getSum() - sumLastSecondAmounts);
-	//		statistic.setAvg(statistic.getSum() / statistic.getCount());
-	//		statistic.setMin(getMinOfAllAmounts());
-	//		statistic.setMax(getMaxOfAllAmounts());
-	//	}
-
-	//	public static Double getMinOfAllAmounts() {
-	//		Double min = 0D;
-	//		//TODO: figure out clever way of obtaining min
-	//		return min;
-	//	}
-	//
-	//	public static Double getMaxOfAllAmounts() {
-	//		Double max = 0D;
-	//		//TODO: figure out clever way of obtaining max
-	//		return max;
-	//	}
-
 }
